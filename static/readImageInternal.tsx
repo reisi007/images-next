2c0ce62dddcd1bc200be5b5691cdba75760ab84b
Florian Reisinger <reisi007@gmail.com>
2022-12-22 18:49:20 +0100

    Prepare for using this repo in next-gallery

--- static/readImageInternal.tsx
+++ static/readImageInternal.tsx
@@ -7,9 +7,8 @@ import {
 import { asyncMap } from '../utils/asyncFlatMap';
 
 export const DIRECTORY_IMAGE = path.join(process.cwd(), 'public', 'images');
-const imageMetadataDirectory = path.join(process.cwd(), 'private', 'images');
 
-export async function readImage(filename: string, imageFolder: string = DIRECTORY_IMAGE): Promise<ImageInfo> {
+export async function readImageInternal(filename: string, imageFolder: string = DIRECTORY_IMAGE, metadataFolder: string = DIRECTORY_IMAGE): Promise<ImageInfo> {
   const imageFile = path.join(imageFolder, `${filename}.jpg`);
   const parser = ExifParserFactory.create(await fs.readFile(imageFile));
   parser.enableBinaryFields(true);
@@ -19,7 +18,7 @@ export async function readImage(filename: string, imageFolder: string = DIRECTOR
   const exif = parser.parse();
   if (exif === undefined) throw Error(`Image ${filename} not found!`);
   const jsonFileName = path.basename(filename);
-  const jsonMetadata: JsonMetadata = JSON.parse(await fs.readFile(path.join(imageMetadataDirectory, `${jsonFileName}.json`), 'utf8'));
+  const jsonMetadata: JsonMetadata = JSON.parse(await fs.readFile(path.join(metadataFolder, `${jsonFileName}.json`), 'utf8'));
   const metadata: Metadata = {
     ...jsonMetadata,
     created: exif.tags?.CreateDate,
@@ -30,7 +29,7 @@ export async function readImage(filename: string, imageFolder: string = DIRECTOR
   };
 }
 
-export async function readMultipleImages<F extends string>(filename: Array<F>): Promise<MetadataMap<F>> {
-  const entries: Array<readonly[F, ImageInfo]> = await asyncMap(filename, async (f: F) => [f, await readImage(f)] as const);
+export async function readMultipleImagesInternal<F extends string>(filename: Array<F>, imageFolder: string = DIRECTORY_IMAGE, metadataFolder: string = DIRECTORY_IMAGE): Promise<MetadataMap<F>> {
+  const entries: Array<readonly[F, ImageInfo]> = await asyncMap(filename, async (f: F) => [f, await readImageInternal(f, imageFolder, metadataFolder)] as const);
   return Object.fromEntries(entries) as MetadataMap<F>;
 }
