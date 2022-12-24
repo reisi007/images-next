4fe501ef3e0c699a5313848bf3310884e544c207
Florian Reisinger <reisi007@gmail.com>
2022-12-24 10:27:41 +0100

    Improve metadata handling

--- types/ImageTypes.tsx
+++ types/ImageTypes.tsx
@@ -1,6 +1,20 @@
-import { ImageSize } from 'ts-exif-parser';
-
-export type ImageInfo = { size: ImageSize, metadata: Metadata };
-export type Metadata = JsonMetadata & { created?: number };
+export type ImageInfo = { size: { width: number, height: number }, metadata: Metadata };
+export type Metadata = JsonMetadata & { created?: string };
 export type JsonMetadata = { title: string, tags: Array<string> };
 export type MetadataMap<F extends string> = Record<F, ImageInfo>;
+
+const EXIF_TAGS = [
+  'CAMERA_MAKE',
+  'CAMERA_MODEL',
+  'LENS_MODEL',
+  'FOCAL_LENGTH',
+  'CREATION_DATETIME',
+  'APERTURE',
+  'SHUTTER_SPEED',
+  'ISO',
+];
+
+type Created = typeof EXIF_TAGS[number];
+export type JsonExifMetadata = {
+  width: number, height: number, exif: Record<Created, string | undefined>
+};
