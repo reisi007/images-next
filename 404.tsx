047596a7602fe1e2fc11e41a16b00c5c28503f9c
Florian Reisinger <reisi007@gmail.com>
2023-01-23 18:04:18 +0100

    Improve website

---
+++ 404.tsx
@@ -0,0 +1,10 @@
+export function Content404() {
+  return (
+    <p className="flex min-h-[50vh] items-center justify-center">
+      <p className="border-r-2 text-2xl font-semibold">404</p>
+      <p className="text-base">
+        Die gesuchte Seite wurde leider nicht gefunden.
+      </p>
+    </p>
+  );
+}
